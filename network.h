603e22dceacdc77861e04a9d242b9426b454a2ca
Petru Motrescu <petru.motrescu@gmail.com>
2018-04-02 15:50:03 +0300

    Dendrites

--- network.h
+++ network.h
@@ -2,13 +2,14 @@
 #define NETWORK_H
 
 #include <vector>
+#include <memory>
 #include <iostream>
 #include <initializer_list>
 #include "neuron.h"
 
-typedef std::vector<Neuron> Layer;
 typedef std::vector<double> Input;
 typedef std::vector<double> Output;
+typedef std::vector<std::shared_ptr<Neuron>> Layer;
 
 class Network
 {
@@ -27,22 +28,48 @@ public:
 
             for (int i = 0; i < count; i++)
             {
-                Neuron neuron;
+                auto neuron = std::make_shared<Neuron>();
                 layer.push_back(neuron);
             }
 
             m_layers.push_back(layer);
         }
+
+        for (int i = 1; i < m_layers.size(); i++)
+        {
+            Layer& previous_layer = m_layers[i - 1];
+            Layer& current_layer = m_layers[i];
+
+            for (auto neuron : current_layer)
+            {
+                neuron->add_dendrites(previous_layer);
+            }
+        }
     }
 
     void learn(Input input, Output output)
     {
-        
+        Layer& input_layer = get_input_layer();
+        for (int i = 0; i < input.size(); i++)
+        {
+            input_layer[i]->set_value(input[i]);
+        }
     }
 
     Output compute(Input input)
     {
         return Output();
+    }
+
+private:
+    Layer& get_input_layer()
+    {
+        return m_layers[0];
+    }
+
+    Layer& get_output_layer()
+    {
+        return m_layers[m_layers.size() - 1];
     }
 
 private:
--- neuron.h
+++ neuron.h
@@ -3,9 +3,38 @@
 #ifndef NEURON_H
 #define NEURON_H
 
+class Neuron;
+
+struct Dendrite
+{
+    std::shared_ptr<Neuron> source;
+    double weight;
+};
+
 class Neuron
 {
 public:
+    void add_dendrite(std::shared_ptr<Neuron> source)
+    {
+        Dendrite dendrite;
+        dendrite.source = source;
+        dendrite.weight = 1.0;
+        m_dendrites.push_back(dendrite);
+    }
+
+    void add_dendrites(std::vector<std::shared_ptr<Neuron>>& sources)
+    {
+        for (auto source : sources)
+        {
+            add_dendrite(source);
+        }
+    }
+
+    void set_value(double value)
+    {
+        m_value = value;
+    }
+
     double get_value()
     {
         return m_value;
@@ -13,6 +42,7 @@ public:
 
 private:
     double m_value;
+    std::vector<Dendrite> m_dendrites;
 };
 
 #endif // NEURON_H
