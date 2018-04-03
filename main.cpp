5b1d4c9b638cb1622aabb562850defc26b809601
Petru Motrescu <petru.motrescu@gmail.com>
2018-04-03 21:34:02 +0300

    Poor man's training digit "bitmaps"

--- main.cpp
+++ main.cpp
@@ -8,21 +8,13 @@ using namespace std;
 
 int main()
 {
-    const int input_count = 2;
-    const int hidden_count = 2;
+    const int input_count = 4;
+    const int hidden_count = 4;
     const int output_count = 1;
 
     Network net{input_count, hidden_count, output_count};
-
-    // for (int i = 0; i < 10; i++)
-    // {
-    //     net.learn({1, 1}, {0});
-    //     net.learn({0, 0}, {0});
-    //     net.learn({1, 0}, {1});
-    //     net.learn({0, 1}, {1});
-    // }
     
-    auto input = std::vector<double>{1, 1};
+    auto input = std::vector<double>{1, 1, 1, 1};
     auto output = net.compute(input);
 
     for (auto value : output)
